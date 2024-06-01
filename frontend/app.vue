09722cc18f3de0239f990518a21697c14dc98611
Tak Castel <takcastel@gmail.com>
2024-06-01 12:14:01 +0200

    preshot landing page

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>
-    Hello Llama Radio
+  <div class="card flex justify-center">
+    hello ?
+    <Sidebar v-model:visible="visible" header="Sidebar">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </Sidebar>
+
+    <Button icon="pi pi-arrow-right" @click="visible = true" />
   </div>
 </template>
+
+<script setup lang="ts">
+import 'primevue/resources/themes/aura-light-green/theme.css'
+
+import Sidebar from 'primevue/sidebar';
+import Button from 'primevue/button';
+
+import { ref } from 'vue';
+const visible = ref(false);
+</script>
