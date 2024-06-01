ab8b96a9dd692d87e4f0d18ab36ed38011853f2b
Tak Castel <takcastel@gmail.com>
2024-06-01 12:14:08 +0200

    nuxt config

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,4 +1,14 @@
+import path from 'path'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  modules: ['nuxt-primevue', "@nuxtjs/tailwindcss"],
+  primevue: {
+      options: {
+        unstyled: true
+      },
+      importPT: { 
+        from: path.resolve(__dirname, './presets/rl-ui/') 
+      } 
+  }
 })
