964bd45a27ae9b02931a360fd11c134df44176c6
Tak Castel <takcastel@gmail.com>
2024-06-01 13:36:52 +0200

    tailwind + preset

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,20 +1,12 @@
 <template>
-  <div class="card flex justify-center">
-    hello ?
-    <Sidebar v-model:visible="visible" header="Sidebar">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-    </Sidebar>
-
-    <Button icon="pi pi-arrow-right" @click="visible = true" />
+  <div>
+    <h1>Radio Llama</h1>
   </div>
 </template>
 
 <script setup lang="ts">
-import 'primevue/resources/themes/aura-light-green/theme.css'
-
-import Sidebar from 'primevue/sidebar';
-import Button from 'primevue/button';
-
-import { ref } from 'vue';
-const visible = ref(false);
 </script>
+
+<style>
+@import url("~/assets/css/base.css");
+</style>
